b7144a7d739b0663304f0cee95673aef4ec8228b
Yuta Ichinohe <>
2024-12-08 21:42:38 +0900

    確認

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -89,9 +89,9 @@
           </div>
           <div class="selected_engineer">
             <label>選択科目</label>
-            <span>国語</span><input type="radio" name="subject" value="1" form="userSearchRequest">
-            <span>数学</span><input type="radio" name="subject" value="2" form="userSearchRequest">
-            <span>英語</span><input type="radio" name="subject" value="3" form="userSearchRequest">
+            <span>国語</span><input type="checkbox" name="subject" value="1" form="userSearchRequest">
+            <span>数学</span><input type="checkbox" name="subject" value="2" form="userSearchRequest">
+            <span>英語</span><input type="checkbox" name="subject" value="3" form="userSearchRequest">
           </div>
         </div>
       </div>
