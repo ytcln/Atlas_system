6cc13e384c0e66331da3c435bebd3890d508945b
Yuta Ichinohe <>
2025-01-29 20:35:12 +0900

    確認お願いいたします。

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -1,7 +1,7 @@
 <x-sidebar>
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
   <div class="w-50 m-auto h-75">
-    <p><span>{{ \Carbon\Carbon::now()->format('Y年m月d') }}日</span><span class="ml-3">部</span></p>
+    <p><span>{{ $date }}日</span><span class="ml-3">{{ $part }}部</span></p>
     <div class="h-75 border">
       <table class="">
         <tr class="text-center">
@@ -9,10 +9,15 @@
           <th class="w-25">名前</th>
           <th class="w-25">場所</th>
         </tr>
+        @foreach($reservePersons as $reservePerson)
+        @foreach($reservePerson->users as $user)
         <tr class="text-center">
-          <td class="w-25"></td>
-          <td class="w-25"></td>
+          <td class="w-25">{{ $user->id }}</td>
+          <td class="w-25">{{ $user->over_name }}{{ $user->under_name }}</td>
+          <td class="w-25">リモート</td>
         </tr>
+        @endforeach
+        @endforeach
       </table>
     </div>
   </div>
